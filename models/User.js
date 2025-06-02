154b9aaf6e33fcb2197bdc7e02beefadde3eb112
naimjalal <naimjalal@gmail.com>
2025-06-02 20:47:35 +0300

    Initial backend setup , MongoDB connection, and User model schema

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema } = require('mongoose')
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  gender: { type: String, enum: ['Male', 'Female'] },
+  isDonor: { type: Boolean, default: false },
+  bloodType: { type: String },
+  nationality: { type: String },
+  dateOfBirth: { type: String },
+  phone: { type: String },
+  allergies: { type: String }
+},
+{timestamps: true}
+)
+
+module.exports = userSchema
+
--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require('express')
+const logger = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
+const db = require('./db')
 
+const PORT = process.env.PORT || 3001
 const app = express()
-// Middleware to parse URL-encoded data from forms (Body Parser)
-app.use(express.urlencoded({ extended: false }))
-
 app.get('/', (req, res) => {
   res.send(' Welcome to the Medappoint API!')
 })
-// Set the port to 3000
-const port = process.env.PORT ? process.env.PORT : '3000'
-mongoose.connect(process.env.MONGODB_URI)
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
-})
-mongoose.connection.on('error', (err) => {
-  console.error(' MongoDB connection error:', err)
-})
-
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
 
 
 
 //listen to port
-app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`)
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
 })
