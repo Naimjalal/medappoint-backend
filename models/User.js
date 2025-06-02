e98e2fb4546697c0ca4b8c9ab07a3bae7d9037b0
naimjalal <naimjalal@gmail.com>
2025-06-02 22:07:36 +0300

    Set up authentication middleware with bcrypt and JWT

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { Schema } = require('mongoose')
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  passwordDigest: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   gender: { type: String, enum: ['Male', 'Female'] },
