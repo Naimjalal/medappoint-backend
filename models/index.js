34025b8320fb9135fb1bd2581f7eccf258ba9ff5
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-06-03 10:05:58 +0300

    Define Appointment model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 const userSchema = require('./User')
 const hospitalSchema = require('./Hospital')
+const appointmentSchema = require('./Appointment')
 
 const User = mongoose.model('User', userSchema)
 const Hospital = mongoose.model('Hospital', hospitalSchema)
+const Appointment = mongoose.model('Appointment', appointmentSchema)
 
-module.exports = { User, Hospital }
+module.exports = { User, Hospital, Appointment }
