f4a22c0a81edbe9228a18d22f57954d2fb204e91
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-06-04 00:14:49 +0300

    Create donations post route

--- controllers/DonationsController.js
+++ controllers/DonationsController.js
@@ -1,3 +1,12 @@
 const { User, Hospital, Donation } = require('../models')
 
-module.exports = {}
+const createDonation = async (req, res) => {
+  try {
+    await Donation.create(req.body)
+    res.status(200).send('Donation stored successfully!')
+  } catch (error) {
+    res.status(404).send('Error storing donation!')
+  }
+}
+
+module.exports = { createDonation }
--- routes/DonationsRouter.js
+++ routes/DonationsRouter.js
@@ -2,4 +2,11 @@ const router = require('express').Router()
 const DonationsController = require('../controllers/DonationsController')
 const middleware = require('../middleware')
 
+router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  DonationsController.createDonation
+)
+
 module.exports = router
