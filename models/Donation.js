f378adb72723c992544f8dbb8e659b436139717a
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-06-03 15:55:23 +0300

    Edit Donation model

--- models/Donation.js
+++ models/Donation.js
@@ -1,14 +1,18 @@
 const { Schema } = require('mongoose')
 
-const donationSchema = new Schema (
-  {
-    
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    }
-  }
-)
+const donationSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  hospitalId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Hospital',
+    required: true
+  },
+  time: { type: String, required: true },
+  isDonated: { type: Boolean, required: true }
+})
 
 module.exports = donationSchema
