1081494f0bd131babc8015fb60a643d329549eae
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-06-04 12:34:44 +0300

    Configure hospital doctors route

--- controllers/HospitalsController.js
+++ controllers/HospitalsController.js
@@ -1,6 +1,19 @@
-const { Hospital } = require('../models')
+const { Hospital, Doctor } = require('../models')
+
 const getHospitals = async (req, res) => {
   const foundHospitals = await Hospital.find({}).populate('departmentsId')
   res.status(200).send(foundHospitals)
 }
-module.exports = { getHospitals }
+
+const getHospitalDoctors = async (req, res) => {
+  const foundDoctors = await Doctor.find({
+    hospitalId: req.params.hospitalId
+  }).populate('departmentId')
+  res
+    .status(200)
+    .send(
+      Object.groupBy(foundDoctors, (doctors) => doctors.departmentId.depName)
+    )
+}
+
+module.exports = { getHospitals, getHospitalDoctors }
--- routes/HospitalsRouter.js
+++ routes/HospitalsRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const HospitalsController = require('../controllers/HospitalsController')
 
 router.get('/', HospitalsController.getHospitals)
+router.get('/:hospitalId', HospitalsController.getHospitalDoctors)
 
 module.exports = router
