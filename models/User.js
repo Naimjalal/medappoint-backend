0660857b469e2abb4f1d16af1599ef9b129378c7
naimjalal <naimjalal@gmail.com>
2025-06-11 15:48:08 +0300

    added cpr validation

--- models/User.js
+++ models/User.js
@@ -1,22 +1,21 @@
 const { Schema } = require('mongoose')
 
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  passwordDigest: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-    cpr: { type: String, required: true }, 
-  gender: { type: String, enum: ['Male', 'Female'] },
-  // isDonor: { type: Boolean, default: false },
-  bloodType: { type: String },
-  nationality: { type: String },
-  dateOfBirth: { type: String },
-  phone: { type: String , required:true},
-  allergies: { type: String }
-},
-{timestamps: true}
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    passwordDigest: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    cpr: { type: String, required: true,match:[/^\d{9}$/,"CPR must be exactly 9 digits"] },
+    gender: { type: String, enum: ['Male', 'Female'] },
+    bloodType: { type: String },
+    nationality: { type: String },
+    dateOfBirth: { type: String },
+    phone: { type: String, required: true },
+    allergies: { type: String }
+  },
+  { timestamps: true }
 )
 
 module.exports = userSchema
-
